4e29e4f643abf0299473f55ada64bc972c33c058
Mahylee <137456382+Mahylee@users.noreply.github.com>
2024-10-24 19:17:57 -0400

    librerias

---
+++ manager.py
@@ -0,0 +1,33 @@
+from tkinter import Tk, Frame
+from container import Container 
+
+class Manager(Tk):
+    def __init__(self, *args, **kwargs ):
+        super().__init__(*args, **kwargs)
+        self.title("Caja registradora version 1.0")
+        self.resizable (False,False)
+        self.configure(bg="#C6D9E3")
+        self.geometry("800x400+12+20")
+        
+        self.container = Frame(self, bg="#C6D9E3")
+        self.container.pack(fill="booth", expand=True)
+        
+        self.frames = {
+            Container: None
+        }
+        
+    def load_frames(self):
+        for FrameClass in self.frames.keys():
+            frame = FrameClass(self.container, self)
+            self.frames[FrameClass] = frame
+            
+    def show_frame(self, frame_class);
+        frame = self.frames[frame_class]
+        frame.tkraise()
+        
+def main():
+    app = Manager()
+    app.mainloop()
+    
+if  __name__== "_main_";
+   main()
